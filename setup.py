9719442503f3e3fb50641f19bf0eeb15d3fae5a1
Alex Couper <alex.couper@glassesdirect.com>
2012-07-19 00:37:46 +0100

    Reset to 0.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='supermutes',
     description='A collection of super mutables',
     long_description=long_description,
-    version='1.0.0',
+    version='0.1.3',
     author='Alex Couper',
     author_email='info@alexcouper.com',
     url='https://github.com/alexcouper/supermutes',
